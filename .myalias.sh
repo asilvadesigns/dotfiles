e4ed6ffa97a8ce0ff38b45b21ce0f3d2a8fd99c3
Alberto Silva <asilvadesigns@gmail.com>
2017-07-10 10:37:28 -0400

    add my alias file

---
+++ .myalias.sh
@@ -0,0 +1,44 @@
+#   GENERAL
+alias gohome='cd ~'
+alias godesktop='cd ~/Desktop'
+alias godotfiles='cd ~/Desktop/Github/dotfiles/'
+alias goexperiments='cd ~/Desktop/Experiments/'
+
+#   PERSONAL
+alias gobunnies='cd ~/Desktop/Github/fightthebunnies'
+alias buildbunnies='cd ~/Desktop/Github/fightthebunnies && gulp'
+alias codebunnies='cd ~/Desktop/Github/fightthebunnies && nvim'
+alias gobunnies='cd ~/Desktop/Github/fightthebunnies'
+alias gogithub='cd ~/Desktop/Github'
+alias gogoogles='cd ~/Desktop/Google'
+alias gosites='cd ~/Sites'
+alias rm='trash'
+
+#   VIM
+alias vim='/usr/local/Cellar/vim/8.0.0160/bin/vim'
+alias govimrc='mvim ~/.vimrc'
+
+#   TELLUS
+alias gotellus='cd ~/Sites/tellusapts.com'
+alias gotelluscode='cd ~/Sites/tellusapts.com/wp-content/themes/uncode-child'
+alias buildtelluscode='cd ~/Sites/tellusapts.com/wp-content/themes/uncode-child/assets && gulp'
+alias nvimtelluscode='cd ~/Sites/tellusapts.com/wp-content/themes/uncode-child && nvim'
+
+#   VYV
+alias govyv='cd ~/Sites/vyvapts.com'
+alias govyvcode='cd ~/Sites/vyvapts.com/wp-content/themes/uncode-child'
+alias buildvyvcode='cd ~/Sites/vyvapts.com/wp-content/themes/uncode-child/assets && gulp'
+alias nvimvyvcode='cd ~/Sites/vyvapts.com/wp-content/themes/uncode-child && nvim'
+
+#   SYRACUSE
+alias gosyracuse='cd ~/Sites/syracuse'
+alias gosyracusecode='cd ~/Sites/syracuse/wp-content/themes/uncode-child'
+alias buildsyracusecode='cd ~/Sites/syracuse/wp-content/themes/uncode-child/assets && gulp'
+alias nvimsyracusecode='cd ~/Sites/syracuse/wp-content/themes/uncode-child && nvim'
+
+#   PURPLETIE
+alias gopurpletie='cd ~/Sites/purple-tie.com'
+alias gopurpletiecode='cd ~/Sites/purple-tie.com/wp-content/themes/uncode-child'
+alias buildpurpletiecode='cd ~/Sites/purple-tie.com/wp-content/themes/uncode-child/assets && gulp'
+alias nvimpurpletiecode='cd ~/Sites/purple-tie.com/wp-content/themes/uncode-child && nvim'
+
